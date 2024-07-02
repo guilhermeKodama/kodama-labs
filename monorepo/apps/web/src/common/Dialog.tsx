2743882bfa98bc27068d80ec97122b9a7c78236c
Guilherme Kodama <guilherme.kodama@gmail.com>
2024-07-02 14:28:30 -0300

    fix(mobile): improve mobile UX form submit

--- monorepo/apps/web/src/common/Dialog.tsx
+++ monorepo/apps/web/src/common/Dialog.tsx
@@ -5,6 +5,15 @@ import DialogActions from "@mui/material/DialogActions";
 import DialogContentText from "@mui/material/DialogContentText";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
+import {
+  AppBar,
+  IconButton,
+  Toolbar,
+  Typography,
+  useMediaQuery,
+  useTheme,
+} from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
 
 type Props = {
   open: boolean;
@@ -13,8 +22,12 @@ type Props = {
 };
 
 const DialogComponent = ({ open, onSubmit, onClose }: Props) => {
+  const theme = useTheme();
+  const fullScreen = useMediaQuery(theme.breakpoints.down("md"));
+
   return (
     <Dialog
+      fullScreen={fullScreen}
       onClose={onClose}
       open={open}
       PaperProps={{
@@ -29,7 +42,28 @@ const DialogComponent = ({ open, onSubmit, onClose }: Props) => {
         },
       }}
     >
-      <DialogTitle>Inscreva-se</DialogTitle>
+      {fullScreen && (
+        <AppBar sx={{ position: "relative" }}>
+          <Toolbar>
+            <IconButton
+              edge="start"
+              color="inherit"
+              onClick={onClose}
+              aria-label="close"
+            >
+              <CloseIcon />
+            </IconButton>
+            <Typography sx={{ ml: 2, flex: 1 }} variant="h6" component="div">
+              Inscreva-se
+            </Typography>
+            <Button type="submit" autoFocus color="inherit">
+              Enviar
+            </Button>
+          </Toolbar>
+        </AppBar>
+      )}
+
+      {!fullScreen && <DialogTitle>Inscreva-se</DialogTitle>}
       <DialogContent>
         <DialogContentText>
           Tenha acesso antecipado a plataforma e comece a automatizar a sua vida
@@ -48,10 +82,12 @@ const DialogComponent = ({ open, onSubmit, onClose }: Props) => {
           variant="outlined"
         />
       </DialogContent>
-      <DialogActions>
-        <Button onClick={onClose}>Cancelar</Button>
-        <Button type="submit">Enviar</Button>
-      </DialogActions>
+      {!fullScreen && (
+        <DialogActions>
+          <Button onClick={onClose}>Cancelar</Button>
+          <Button type="submit">Enviar</Button>
+        </DialogActions>
+      )}
     </Dialog>
   );
 };
