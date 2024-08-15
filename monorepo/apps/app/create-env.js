7ff30799a7f4938d494f3936975b6e40d2e0331b
Guilherme Kodama <guilherme.kodama@gmail.com>
2024-08-15 07:29:37 -0300

    fix: add more logs

--- monorepo/apps/app/create-env.js
+++ monorepo/apps/app/create-env.js
@@ -2,7 +2,7 @@ import { writeFileSync } from 'fs';
 import { resolve } from 'path';
 
 // Log to confirm the script is running
-console.log('Starting create-env.js script...');
+console.log('[🍓]', 'Starting create-env.js script...');
 
 // Define the environment variables you want to write
 const envVariables = `
@@ -11,18 +11,16 @@ VITE_ASSET_URL=${process.env.VITE_ASSET_URL}
 `;
 
 // Log the environment variables to confirm they are being read
-console.log('Environment Variables:');
-console.log(`VITE_SERVER_URL=${process.env.VITE_SERVER_URL}`);
-console.log(`VITE_ASSET_URL=${process.env.VITE_ASSET_URL}`);
-
-throw new Error('CREATE SCRIPT WAS EXECUTED');
+console.log('[🍓]', 'Environment Variables:');
+console.log('[🍓]', `VITE_SERVER_URL=${process.env.VITE_SERVER_URL}`);
+console.log('[🍓]', `VITE_ASSET_URL=${process.env.VITE_ASSET_URL}`);
 
 // Write the .env file in the correct directory
 const envFilePath = resolve('./.env'); // Adjust path as needed
 
 try {
   writeFileSync(envFilePath, envVariables);
-  console.log(`.env file created successfully at ${envFilePath}`);
+  console.log('[🍓]', `.env file created successfully at ${envFilePath}`);
 } catch (err) {
-  console.error('Error writing .env file:', err);
+  console.error('[🍓]', 'Error writing .env file:', err);
 }
