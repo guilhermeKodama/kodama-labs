6993dc53919a0f9a150322487d52075288fbc25c
Guilherme Kodama <guilherme.kodama@gmail.com>
2024-09-10 08:30:33 -0300

    fix: cache control vulnerability

--- monorepo/apps/api/src/main.ts
+++ monorepo/apps/api/src/main.ts
@@ -7,9 +7,18 @@ import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.int
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Set global security headers including X-Content-Type-Options
   app.use((req, res, next) => {
+    // Set global security headers including X-Content-Type-Options
     res.setHeader('X-Content-Type-Options', 'nosniff');
+    // Add Anti-clickjacking Headers
+    res.setHeader('X-Frame-Options', 'DENY');
+    // Add Cache-Control headers to prevent caching of sensitive content
+    res.setHeader(
+      'Cache-Control',
+      'no-store, no-cache, must-revalidate, private',
+    );
+    res.setHeader('Pragma', 'no-cache'); // For HTTP/1.0 caches
+    res.setHeader('Expires', '0'); // For HTTP/1.0 caches
     next();
   });
 
