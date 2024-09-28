4a914d0a2fb0285f0c99637971af05136078ee40
Guilherme Kodama <guilherme.kodama@gmail.com>
2024-09-28 15:24:47 -0300

    fix: change socketio to long pooling

--- monorepo/apps/app/src/utils/socketio.ts
+++ monorepo/apps/app/src/utils/socketio.ts
@@ -1,7 +1,8 @@
 import { io } from 'socket.io-client';
 import { CONFIG } from 'src/config-global';
 
-const connect = (context: { query: { userId: string } }) => io(CONFIG.site.serverUrl, context);
+const connect = (context: { query: { userId: string } }) =>
+  io(CONFIG.site.serverUrl, { ...context, transports: ['polling'] });
 
 export default {
   connect,
