7810e2894c77053db38b43826428da6c5bcfa4ae
Guilherme Kodama <guilherme.kodama@gmail.com>
2025-04-29 19:05:28 -0300

    fix: build

--- monorepo/apps/api/src/main.ts
+++ monorepo/apps/api/src/main.ts
@@ -1,11 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ExpressAdapter } from '@nestjs/platform-express';
-import * as express from 'express';
+import express from 'express';
 
-const server = express();
-
-async function bootstrap() {
+// For Vercel
+export default async function handler(req: any, res: any) {
+  const server = express();
   const app = await NestFactory.create(
     AppModule,
     new ExpressAdapter(server)
@@ -28,19 +28,35 @@ async function bootstrap() {
   });
 
   await app.init();
-
-  // For local development
-  if (process.env.NODE_ENV !== 'production') {
-    const port = process.env.PORT || 4000;
-    await app.listen(port);
-    console.log(`Application is running on: ${port}`);
-  }
-
-  return server;
+  return server(req, res);
 }
 
-// For Vercel
-export default async function handler(req: any, res: any) {
-  const app = await bootstrap();
-  return app(req, res);
+// For local development
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.use((req, res, next) => {
+    res.setHeader('X-Content-Type-Options', 'nosniff');
+    res.setHeader('X-Frame-Options', 'DENY');
+    res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, private');
+    res.setHeader('Pragma', 'no-cache');
+    res.setHeader('Expires', '0');
+    next();
+  });
+
+  app.enableCors({
+    origin: ['https://app.wallex.com.br', 'http://localhost:8081'],
+    methods: 'GET,POST,PUT,DELETE',
+    allowedHeaders: 'Content-Type,Authorization',
+    credentials: true,
+  });
+
+  const port = process.env.PORT || 4000;
+  await app.listen(port);
+  console.log(`Application is running on: ${port}`);
+}
+
+// Only run bootstrap() if we're not in Vercel
+if (process.env.NODE_ENV !== 'production') {
+  bootstrap();
 }
